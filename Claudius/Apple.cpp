f491de49e5fa3723b300e9a9792f42c56cccfb44
CaseyDeCoder <hofland.casey@gmail.com>
2021-12-09 13:26:22 +0100

    Remove unnecessary comments

--- Claudius/Apple.cpp
+++ Claudius/Apple.cpp
@@ -12,12 +12,9 @@ Apple::Apple()
 
 void Apple::Initialize(int width, int height)
 {
-	//this->width = width;
-	//this->height = height;
 	color = { 0, 255, 0, 0 };
 	rect = { 0, 0, width, height };
 	trans.SetPosition(100, 200);
-	//direction = { -0.05f, 0.05f };
 }
 
 void Apple::Render(RenderManager& renderManager)
